6c83153275e47abb19980d9328fafa6c1f040a00
Gilles Claessens <claessensgilles@outlook.com>
2023-01-19 20:18:10 +0100

    P1 - W3 Initialized

--- P2-2022/P1-W3/03_02_Enterprise.sql
+++ P2-2022/P1-W3/03_02_Enterprise.sql
@@ -1,18 +1,36 @@
 -- EX. 1
-SELECT distinct 'Jochems', 'maastricht', e2.last_name, initcap(e2.location)
-FROM employees e
-         JOIN employees e2 on e.last_name = e2.last_name
-WHERE upper(e2.location) != 'MAASTRICHT'
-  and e2.gender = 'M';
+SELECT e.last_name AS "last_name JOCHEMS",
+       e.location  AS "city JOCHEMS",
+       e2.last_name,
+       e2.location AS city
+FROM employees e,
+     employees e2
+WHERE e.last_name = 'Jochems'
+  AND e.location != e2.location
+  AND e2.gender = 'M';
 
 -- EX. 2
-SELECT e.employee_id, e.last_name, e.birth_date
-FROM employees e
-         JOIN employees e2 on e.birth_date <> e2.birth_date
-WHERE date_part('month', e2.birth_date) = date_part('month', e.birth_date)
-ORDER BY date_part('month', e2.birth_date);
+SELECT e.employee_id, e.last_name, e2.birth_date
+FROM employees e,
+     employees e2
+WHERE e.employee_id <> e2.employee_id
+  AND DATE_PART('mon', e.birth_date) = DATE_PART('mon', e2.birth_date)
+ORDER BY DATE_PART('mon', e.birth_date);
 
 -- EX. 3
 SELECT p.project_id, p.project_name, p.location, p.department_id
-FROM projects p
-         JOIN projects p1 on p.department_id = p1.department_id;
+FROM projects p,
+     projects p1
+WHERE p.project_id <> 3
+  AND p1.project_id = 3
+  AND p.department_id = p1.department_id;
+
+-- EX. 4
+SELECT e.last_name  AS employee,
+       e1.last_name AS boss,
+       e2.last_name AS uberboss
+FROM employees e,
+     employees e1,
+     employees e2
+WHERE e.manager_id = e1.employee_id
+  AND e1.manager_id = e2.employee_id;
