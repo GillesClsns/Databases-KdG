ad2f45503f0e765affb0cafdec976e6256c700a1
Gilles Claessens <claessensgilles@outlook.com>
2023-01-19 18:10:53 +0100

    P2 - W8 Initialized

--- P2-2022/P2-W8/08_02_Views.sql
+++ P2-2022/P2-W8/08_02_Views.sql
@@ -35,3 +35,70 @@ ORDER BY salary desc;
 
 SELECT *
 FROM v_emp_salary;
+
+-- EX. 4A
+CREATE OR REPLACE VIEW v_department AS
+SELECT *
+FROM departments;
+
+-- EX. 4B
+ALTER TABLE departments
+    ADD COLUMN dept_telnr CHAR(9)
+
+-- EX. 4C
+SELECT *
+FROM v_department;
+SELECT *
+FROM departments;
+
+SELECT view_definition
+FROM INFORMATION_SCHEMA.views
+WHERE table_name = 'v_department';
+
+-- EX. 4D
+CREATE OR REPLACE VIEW v_department AS
+SELECT *
+FROM departments;
+
+-- EX. 4E
+ALTER TABLE departments
+    DROP COLUMN dept_telnr cascade;
+
+-- EX. 4F
+CREATE OR REPLACE VIEW v_department AS
+SELECT *
+FROM departments;
+
+-- EX. 5
+CREATE OR REPLACE VIEW v_emp_salary AS
+SELECT employee_id, first_name, last_name, salary, department_id
+FROM employees
+WHERE department_id = 7
+ORDER BY salary desc;
+
+-- EX. 6
+--< Look it up yourself >--
+
+-- EX. 7
+INSERT INTO v_emp_salary
+VALUES ('999999999', 'Jan', 'Janssens', 35000, 3);
+SELECT *
+FROM v_emp_salary;
+
+-- EX. 8
+--< Look it up yourself >--
+
+-- EX. 9
+CREATE OR REPLACE VIEW v_percentage_proportion AS
+SELECT employee_id,
+       project_id,
+       round((hours / (SELECT sum(hours) FROM tasks WHERE project_id = t.project_id GROUP BY project_id)) * 100)
+FROM tasks t
+ORDER BY project_id, employee_id;
+
+SELECT * FROM v_percentage_proportion;
+
+
+
+
+
